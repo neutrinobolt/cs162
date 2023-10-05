491e59e699f55f97aaaf864927a9a77ca3829f66
neutrinobolt <bescsn@gmail.com>
2023-10-05 16:13:32 -0700

    Updated combatant_test

--- assign_1/combatant_test.py
+++ assign_1/combatant_test.py
@@ -95,6 +95,12 @@ class Player:
         return  "Current Inventory:\n" \
                 f"{inventory_pretty}"
 
+        
+    
+    def obtain(self, item: str):
+        """Adds given item to player inventory."""
+        self.inventory.append(item)
+
 #Get character name from user, initialize player
 loop = 1
 while loop == 1:
@@ -123,6 +129,49 @@ open_selection = random.sample(starting_weapons, 2)
 
 print("Please choose your starting weapon:")
 select_num = 0
+weapon_spread = []
 for option in open_selection:
     print(f"{select_num}: {option}")
     select_num +=1
+    weapon_spread.append(option)
+
+loop = 1
+while loop == 1:
+    try:
+        player_decision = int(input())
+    except ValueError:
+        print("ERROR, invalid input. Please try again:")
+    except:
+        print("ERROR, something unexpected occurred. Please try again:")
+    if player_decision <= 1:
+        new_player.obtain(weapon_spread[player_decision])
+        loop = 0
+    else:
+        print("ERROR, invalid input. Please try again:")
+
+#Allow user to either view stats, view inventory, or exit program.
+
+
+
+loop = 1
+while loop == 1:
+    print("-" * 50)
+    print(f"Main Menu\n\t0: exit \n\t1: View inventory\n\t2:view {chosen_name}'s stats")    
+    try:
+        player_decision = int(input())
+    except ValueError:
+        print("ERROR, invalid input. Please try again:")
+    except:
+        print("ERROR, something unexpected occurred. Please try again:")
+    if player_decision <= 2:
+        if player_decision == 0:
+            loop = 0
+        elif player_decision == 1:
+            print(new_player.view_inventory())
+            input("Press enter to return to Main Menu")
+        elif player_decision == 2:
+            print(new_player.view_stats())
+            input("Press enter to return to Main Menu")
+
+    else:
+        print("ERROR, invalid input. Please try again:")
