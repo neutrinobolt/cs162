bf31d83fb43e10956cb592e0811b9b8ff0740d3a
neutrinobolt <bescsn@gmail.com>
2023-10-24 15:14:37 -0700

    Started work on NewFile class

--- assign_3/main.py
+++ assign_3/main.py
@@ -4,13 +4,20 @@ import gui_lib as gl
 
 def main():
 
-    #Opening window
+    #iterate windows
     start = gl.Start()
     file_select = gl.FileList()
     file_select.file_window.root.withdraw()
     new_file = gl.NewFile()
+    new_file.creator_window.root.withdraw()
+
+    #pair windows
     start.window_connect(file_select.file_window)
-    file_select.window_connect(start.start_window)
+    file_select.start_connect(start.start_window)
+    file_select.creator_connect(new_file.creator_window)
+    new_file.filewin_connect(file_select)
+
+    #Start mainloop
     start.start_window.mainloop()
 
 
